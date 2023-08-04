f437d55a87900b1f9155caf124405d3cb9aaca9b
Mark H Engelmann <engel653@umn.edu>
2023-08-04 15:46:57 -0500

    Add PDF to works, newLinks JSON, and formatting

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,11 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/styles');
   eleventyConfig.addPassthroughCopy('./src/scripts');
   eleventyConfig.addPassthroughCopy('./src/images');
+  eleventyConfig.addPassthroughCopy('./src/scores');
   eleventyConfig.addWatchTarget('./src/styles');
   eleventyConfig.addWatchTarget('./src/scripts');
   eleventyConfig.addWatchTarget('./src/images');
+  eleventyConfig.addWatchTarget('./src/scores');
 
   return {
     dir: {
