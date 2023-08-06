4f5d24c993258c016b0a4ae1b1e23a909d68d6b3
Mark Engelmann <engelmann.mark@gmail.com>
2023-08-06 16:39:12 -0500

    Add pathPrefix plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+
   eleventyConfig.addPassthroughCopy('./src/styles');
   eleventyConfig.addPassthroughCopy('./src/scripts');
   eleventyConfig.addPassthroughCopy('./src/images');
